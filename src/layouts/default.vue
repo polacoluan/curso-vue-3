13fbaee5b9723542592e829eb61df8454079f51b
luan_carvalho <luan_carvalho@econeteditora.com.br>
2024-06-17 13:16:56 -0300

    Primeiro commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.png" subtitle="john@google.com"
+          title="John Leider">
+          <template v-slot:append>
+            <v-btn icon="mdi-menu-down" size="small" variant="text"></v-btn>
+          </template>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list :lines="false" density="compact" nav>
+        <router-link v-for="(item, i) in items" :key="i" :value="item" :to="item.to">
+          <v-list-item  color="primary">
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(null);
+
+const items = [
+  { text: 'My Files', icon: 'mdi-folder', to: '/' },
+  { text: 'Shared with me', icon: 'mdi-account-multiple', to: 'about' },
+];
+
+</script>
